7d6c9cc85f6aa4e499ff5e3304666f2fe1a8b9bd
Katlyn W. <krw8ateez@gmail.com>
2024-08-31 11:23:15 -0400

    Add project files.

---
+++ Chapter2Ex10_Katlyn_Winkelhake.cpp
@@ -0,0 +1,26 @@
+/*
+Title: Chapter 2 Exercise 10
+File Name: Chapter2Ex10_Katlyn_Winkelhake.cpp
+Programmer: Katlyn Winkelhake
+Date: Aug 31 2023
+Requirements:
+A car holds 15 gallons of gasoline and can travel 375 miles before refueling. Write a program that calculates the number of miles per gallon the car gets. Display the result on the screen.
+
+Hint: Use the following formula to calculate miles per gallon (MPG):
+
+MPG = Miles Driven/Gallons of Gas Used
+Once you have coded this and placed in your main branch in GitHub, create a second branch that revises your code so
+
+that the user can enter the number of gallons and the miles driven.
+
+*/
+
+#include <iostream>
+
+int main()
+{
+
+
+
+
+}
